74ee995c943c96593241649521337ec0236f61f7
Southball <jamie.choi.mail.2002@gmail.com>
2022-02-13 22:50:19 +0900

    🎉 Initial commit

---
+++ packages/api/src/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Account';
+export * from './AccountUpdate';
+export * from './ChatMetadata';
+export * from './InlineObject';
+export * from './InlineObject1';
+export * from './InlineResponse200';
+export * from './InlineResponse2001';
+export * from './Message';
+export * from './User';
